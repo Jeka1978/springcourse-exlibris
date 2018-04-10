d436d2d5ad9087b28f5de47d3950ebefa5eb9e48
bsevgeny@gmail.com <papakita2009>
2018-04-10 11:03:30 +0300

    end of day 2

--- src/main/java/personal/mail/MailSender.java
+++ src/main/java/personal/mail/MailSender.java
@@ -1,11 +1,14 @@
 package personal.mail;
 
+import org.springframework.stereotype.Service;
+
 import java.util.HashMap;
 import java.util.Map;
 
 /**
  * @author Evgeny Borisov
  */
+@Service
 public class MailSender {
     private MailDao mailDao = new MailDaoImpl();
 
